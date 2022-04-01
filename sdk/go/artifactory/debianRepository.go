968f6e9f3f5432641a36d296aafa486b60862b05
Paul Stack <public@paulstack.co.uk>
2022-04-01 19:32:54 +0300

    test

--- sdk/go/artifactory/debianRepository.go
+++ sdk/go/artifactory/debianRepository.go
@@ -13,7 +13,7 @@ import (
 
 // ## # Artifactory Local Debian Repository Resource
 //
-// Creates a local Debian repository and allows for the creation of a GPG key
+// Creates a local Debian repository and allows for the creation of a GPG key - testing this out!
 //
 // ## Example Usage
 //
