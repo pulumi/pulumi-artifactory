4601074d552a217900036e62ab524f80e7a972c1
Ian Wahbe <ian@wahbe.com>
2023-01-17 10:21:21 -0800

    Update examples for free plan
    Since we have moved from a free trial to a perpetually free plan, we no
    longer have access to projects. This causes us receive emit "default"
    project keys. Without adding `project_key="default"`, we get diffs after
    updates.

--- examples/local-generic-repository/py/__main__.py
+++ examples/local-generic-repository/py/__main__.py
@@ -1,8 +1,7 @@
 """A Python Pulumi program"""
 
-import pulumi
-
 import pulumi_artifactory as artifactory
 
-my_local = artifactory.LocalGenericRepository("pylumipus-py",
-            key="pylumipus")
+my_local = artifactory.LocalGenericRepository(
+    "pylumipus-py", key="pylumipus", project_key="default"
+)
